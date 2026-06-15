80e751b652fdf3b0e8c50a29a33ae928630fdb6f
hitycc <2685639055@qq.com>
2026-06-15 21:43:35 +0800

    第一次提交：上传VS2019编译的Winform上位机软件

---
+++ EVBHeaterControl/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace EVBHeaterControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetData = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblVoltage = new System.Windows.Forms.Label();
+            this.lblCurrent = new System.Windows.Forms.Label();
+            this.lblPower = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGetData
+            // 
+            this.btnGetData.Location = new System.Drawing.Point(610, 534);
+            this.btnGetData.Name = "btnGetData";
+            this.btnGetData.Size = new System.Drawing.Size(165, 72);
+            this.btnGetData.TabIndex = 0;
+            this.btnGetData.Text = "获取数据";
+            this.btnGetData.UseVisualStyleBackColor = true;
+            this.btnGetData.Click += new System.EventHandler(this.btnGetData_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(311, 534);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(165, 72);
+            this.btnDisconnect.TabIndex = 1;
+            this.btnDisconnect.Text = "断开连接";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(50, 534);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(165, 72);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "连接设备";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblVoltage
+            // 
+            this.lblVoltage.AutoSize = true;
+            this.lblVoltage.Location = new System.Drawing.Point(642, 211);
+            this.lblVoltage.Name = "lblVoltage";
+            this.lblVoltage.Size = new System.Drawing.Size(142, 24);
+            this.lblVoltage.TabIndex = 3;
+            this.lblVoltage.Text = "电压：-- mV";
+            // 
+            // lblCurrent
+            // 
+            this.lblCurrent.AutoSize = true;
+            this.lblCurrent.Location = new System.Drawing.Point(363, 211);
+            this.lblCurrent.Name = "lblCurrent";
+            this.lblCurrent.Size = new System.Drawing.Size(142, 24);
+            this.lblCurrent.TabIndex = 4;
+            this.lblCurrent.Text = "电流：-- uA";
+            // 
+            // lblPower
+            // 
+            this.lblPower.AutoSize = true;
+            this.lblPower.Location = new System.Drawing.Point(93, 211);
+            this.lblPower.Name = "lblPower";
+            this.lblPower.Size = new System.Drawing.Size(142, 24);
+            this.lblPower.TabIndex = 5;
+            this.lblPower.Text = "功率：-- mW";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(860, 699);
+            this.Controls.Add(this.lblPower);
+            this.Controls.Add(this.lblCurrent);
+            this.Controls.Add(this.lblVoltage);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnGetData);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGetData;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblVoltage;
+        private System.Windows.Forms.Label lblCurrent;
+        private System.Windows.Forms.Label lblPower;
+    }
+}
+
---
+++ EVBHeaterControl/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EVBHeaterControl
+{
+    public partial class Form1 : Form
+    {
+        private readonly SFP_EVB_Heater _heater;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _heater = new SFP_EVB_Heater();
+            // 初始禁用断开、获取按钮
+            btnDisconnect.Enabled = false;
+            btnGetData.Enabled = false;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            bool ok = _heater.Open();
+            if (ok)
+            {
+                MessageBox.Show("设备连接成功！");
+                btnConnect.Enabled = false;
+                btnDisconnect.Enabled = true;
+                btnGetData.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("连接失败，请检查设备IP与线路");
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            _heater.Close();
+            MessageBox.Show("已断开设备连接");
+
+            btnConnect.Enabled = true;
+            btnDisconnect.Enabled = false;
+            btnGetData.Enabled = false;
+
+            lblPower.Text = "功率：-- mW";
+            lblCurrent.Text = "电流：-- uA";
+            lblVoltage.Text = "电压：-- mV";
+        }
+
+        private void btnGetData_Click(object sender, EventArgs e)
+        {
+            if (!_heater.IsOpen)
+            {
+                MessageBox.Show("请先连接设备！");
+                return;
+            }
+            string power = _heater.GetPower();
+            string current = _heater.GetCurrent();
+            string voltage = _heater.GetVoltage();
+
+            lblPower.Text = $"功率：{(power ?? "无数据")} mW";
+            lblCurrent.Text = $"电流：{(current ?? "无数据")} uA";
+            lblVoltage.Text = $"电压：{(voltage ?? "无数据")} mV";
+        }
+    }
+}
