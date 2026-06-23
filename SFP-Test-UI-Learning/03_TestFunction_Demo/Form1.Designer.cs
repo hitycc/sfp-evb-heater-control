0194f4f67449debc05db4e98bf0bd52d34abd26a
hitycc <2685639055@qq.com>
2026-06-23 15:10:57 +0800

    新增03_TestFunction_Demo文件

---
+++ SFP-Test-UI-Learning/03_TestFunction_Demo/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace _03_TestFunction_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblDevStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblTestStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblTime = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timerStatus = new System.Windows.Forms.Timer(this.components);
+            this.flowBtnTop = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnStartTest = new System.Windows.Forms.Button();
+            this.btnSaveTxtLog = new System.Windows.Forms.Button();
+            this.btnSaveCsvReport = new System.Windows.Forms.Button();
+            this.panelProgress = new System.Windows.Forms.Panel();
+            this.progressTest = new System.Windows.Forms.ProgressBar();
+            this.splitMain = new System.Windows.Forms.SplitContainer();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.toolTipHelp = new System.Windows.Forms.ToolTip(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.flowBtnTop.SuspendLayout();
+            this.panelProgress.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitMain)).BeginInit();
+            this.splitMain.Panel2.SuspendLayout();
+            this.splitMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblDevStatus,
+            this.lblTestStatus,
+            this.lblTime});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblDevStatus
+            // 
+            this.lblDevStatus.Name = "lblDevStatus";
+            this.lblDevStatus.Size = new System.Drawing.Size(80, 17);
+            this.lblDevStatus.Text = "设备：未连接";
+            // 
+            // lblTestStatus
+            // 
+            this.lblTestStatus.Name = "lblTestStatus";
+            this.lblTestStatus.Size = new System.Drawing.Size(68, 17);
+            this.lblTestStatus.Text = "测试：空闲";
+            // 
+            // lblTime
+            // 
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(110, 17);
+            this.lblTime.Text = "时间：00：00：00";
+            // 
+            // timerStatus
+            // 
+            this.timerStatus.Enabled = true;
+            this.timerStatus.Interval = 1000;
+            this.timerStatus.Tick += new System.EventHandler(this.timerStatus_Tick);
+            // 
+            // flowBtnTop
+            // 
+            this.flowBtnTop.BackColor = System.Drawing.Color.Chartreuse;
+            this.flowBtnTop.Controls.Add(this.btnStartTest);
+            this.flowBtnTop.Controls.Add(this.btnSaveTxtLog);
+            this.flowBtnTop.Controls.Add(this.btnSaveCsvReport);
+            this.flowBtnTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowBtnTop.Location = new System.Drawing.Point(0, 0);
+            this.flowBtnTop.Name = "flowBtnTop";
+            this.flowBtnTop.Size = new System.Drawing.Size(800, 36);
+            this.flowBtnTop.TabIndex = 1;
+            // 
+            // btnStartTest
+            // 
+            this.btnStartTest.Location = new System.Drawing.Point(3, 3);
+            this.btnStartTest.Name = "btnStartTest";
+            this.btnStartTest.Size = new System.Drawing.Size(99, 23);
+            this.btnStartTest.TabIndex = 0;
+            this.btnStartTest.Text = "开始批量测试";
+            this.toolTipHelp.SetToolTip(this.btnStartTest, "模拟多通道SFP自动测试，进度条实时更新测试进度");
+            this.btnStartTest.UseVisualStyleBackColor = true;
+            this.btnStartTest.Click += new System.EventHandler(this.btnStartTest_Click);
+            // 
+            // btnSaveTxtLog
+            // 
+            this.btnSaveTxtLog.Location = new System.Drawing.Point(108, 3);
+            this.btnSaveTxtLog.Name = "btnSaveTxtLog";
+            this.btnSaveTxtLog.Size = new System.Drawing.Size(98, 23);
+            this.btnSaveTxtLog.TabIndex = 1;
+            this.btnSaveTxtLog.Text = "保存TXT日志";
+            this.toolTipHelp.SetToolTip(this.btnSaveTxtLog, "将本次运行全部日志保存到按日期分类的本地文件夹");
+            this.btnSaveTxtLog.UseVisualStyleBackColor = true;
+            this.btnSaveTxtLog.Click += new System.EventHandler(this.btnSaveTxtLog_Click);
+            // 
+            // btnSaveCsvReport
+            // 
+            this.btnSaveCsvReport.Location = new System.Drawing.Point(212, 3);
+            this.btnSaveCsvReport.Name = "btnSaveCsvReport";
+            this.btnSaveCsvReport.Size = new System.Drawing.Size(115, 23);
+            this.btnSaveCsvReport.TabIndex = 2;
+            this.btnSaveCsvReport.Text = "导出CSV测试报告";
+            this.toolTipHelp.SetToolTip(this.btnSaveCsvReport, "导出本次所有模块电压、温度测试数据，生成Excel可打开的CSV表格");
+            this.btnSaveCsvReport.UseVisualStyleBackColor = true;
+            this.btnSaveCsvReport.Click += new System.EventHandler(this.btnSaveCsvReport_Click);
+            // 
+            // panelProgress
+            // 
+            this.panelProgress.Controls.Add(this.progressTest);
+            this.panelProgress.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelProgress.Location = new System.Drawing.Point(0, 36);
+            this.panelProgress.Name = "panelProgress";
+            this.panelProgress.Size = new System.Drawing.Size(800, 60);
+            this.panelProgress.TabIndex = 2;
+            // 
+            // progressTest
+            // 
+            this.progressTest.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressTest.Location = new System.Drawing.Point(0, 0);
+            this.progressTest.Margin = new System.Windows.Forms.Padding(10);
+            this.progressTest.Name = "progressTest";
+            this.progressTest.Size = new System.Drawing.Size(800, 60);
+            this.progressTest.TabIndex = 0;
+            // 
+            // splitMain
+            // 
+            this.splitMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitMain.Location = new System.Drawing.Point(0, 96);
+            this.splitMain.Name = "splitMain";
+            this.splitMain.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitMain.Panel2
+            // 
+            this.splitMain.Panel2.Controls.Add(this.rtbLog);
+            this.splitMain.Size = new System.Drawing.Size(800, 332);
+            this.splitMain.SplitterDistance = 80;
+            this.splitMain.TabIndex = 3;
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbLog.Location = new System.Drawing.Point(0, 0);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.ReadOnly = true;
+            this.rtbLog.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.rtbLog.Size = new System.Drawing.Size(800, 248);
+            this.rtbLog.TabIndex = 0;
+            this.rtbLog.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitMain);
+            this.Controls.Add(this.panelProgress);
+            this.Controls.Add(this.flowBtnTop);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.flowBtnTop.ResumeLayout(false);
+            this.panelProgress.ResumeLayout(false);
+            this.splitMain.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitMain)).EndInit();
+            this.splitMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblDevStatus;
+        private System.Windows.Forms.ToolStripStatusLabel lblTestStatus;
+        private System.Windows.Forms.ToolStripStatusLabel lblTime;
+        private System.Windows.Forms.Timer timerStatus;
+        private System.Windows.Forms.FlowLayoutPanel flowBtnTop;
+        private System.Windows.Forms.Button btnStartTest;
+        private System.Windows.Forms.ToolTip toolTipHelp;
+        private System.Windows.Forms.Button btnSaveTxtLog;
+        private System.Windows.Forms.Button btnSaveCsvReport;
+        private System.Windows.Forms.Panel panelProgress;
+        private System.Windows.Forms.ProgressBar progressTest;
+        private System.Windows.Forms.SplitContainer splitMain;
+        private System.Windows.Forms.RichTextBox rtbLog;
+    }
+}
+
---
+++ SFP-Test-UI-Learning/03_TestFunction_Demo/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _03_TestFunction_Demo
+{
+    public partial class Form1 : Form
+    {
+        // 存储模拟测试数据列表，用于导出CSV报告
+        // 每条数据：模块编号、电压、温度、测试结果
+        private List<string[]> testDataList = new List<string[]>();
+        public Form1()
+        {
+            InitializeComponent();
+            // 初始化：自动创建今日日志文件夹
+            CreateLogFolder();
+            // 启动计时器，刷新状态栏时间
+            timerStatus.Start();
+        }
+        /// <summary>
+        /// 程序启动自动生成日志文件夹，格式：Logs/2026-06-23
+        /// Path类：专门处理文件路径，跨系统兼容，不用手动拼接斜杠
+        /// </summary>
+        private void CreateLogFolder()
+        {
+            // 获取程序运行根目录
+            string basePath = Application.StartupPath;
+            // 拼接Logs总文件夹路径
+            string logRoot = Path.Combine(basePath, "Logs");
+            // 获取当天日期字符串 yyyy-MM-dd
+            string today = DateTime.Now.ToString("yyyy-MM-dd");
+            // 拼接当日日志文件夹完整路径
+            string todayLogPath = Path.Combine(logRoot, today);
+            // 判断文件夹不存在则创建
+            if (!Directory.Exists(todayLogPath))
+            {
+                Directory.CreateDirectory(todayLogPath);
+            }
+            // 存储全局，后续保存文件使用
+            GlobalLogPath = todayLogPath;
+        }
+        // 全局存储当日日志路径
+        private string GlobalLogPath = "";
+        /// <summary>
+        /// 每秒执行一次，刷新状态栏系统时间
+        /// </summary>
+        private void timerStatus_Tick(object sender, EventArgs e)
+        {
+            string nowTime = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+            lblTime.Text = $"时间：{nowTime}";
+        }
+
+        private void btnStartTest_Click(object sender, EventArgs e)
+        {
+            // 重置进度条、测试数据
+            progressTest.Value = 0;
+            testDataList.Clear();
+            lblTestStatus.Text = "测试：运行中";
+            AddLog("===== 开始批量SFP模块自动测试 =====");
+            // 模拟10个光模块循环测试
+            int totalModule = 10;
+            for (int i = 1; i <= totalModule; i++)
+            {
+                // 模拟读取硬件数据
+                string moduleId = $"SFP-{i:D2}";
+                string volt = (3.2 + i * 0.5).ToString("0.00");
+                string temp = (25 + i).ToString("0.0");
+                string result = "合格";
+                //存入测试数据集合，用于导出报告
+                testDataList.Add(new string[] { moduleId, volt, temp, result });
+                // 更新进度条
+                progressTest.Value = i * 100 / totalModule;
+                AddLog($"测试模块{moduleId}|电压：{volt}V|温度{temp}℃|{result}");
+                // 模拟硬件读取延时，肉眼可见进度条走动
+                System.Threading.Thread.Sleep(200);
+                // 刷新界面，防止窗口假死
+                Application.DoEvents();
+            }
+            lblTestStatus.Text = "测试：完成";
+            AddLog("===== 全部模块测试完成 =====");
+        }
+        private void AddLog(string msg)
+        {
+            string time = DateTime.Now.ToString("yyyy-MM-dd");
+            string logText = $"[{time}]{msg}\r\n";
+            rtbLog.AppendText(logText);
+            rtbLog.SelectionStart = rtbLog.TextLength;
+            rtbLog.ScrollToCaret();
+        }
+
+        private void btnSaveTxtLog_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 拼接日志文件名：时:分:秒.txt
+                string fileName = DateTime.Now.ToString("yyyy-MM-dd")+"_运行日志.txt";
+                string fullPath = Path.Combine(GlobalLogPath, fileName);
+                // 创建写入流，写入日志框全部内容
+                using (StreamWriter sw = new StreamWriter(fullPath, false, Encoding.UTF8))
+                {
+                    sw.Write(rtbLog.Text);
+                }
+                MessageBox.Show($"TXT日志保存成功！\n路径：{fullPath}", "保存成功");
+                AddLog($"已保存成功文件{fileName}");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"保存失败{ex.Message}", "错误");
+            }
+        }
+
+        private void btnSaveCsvReport_Click(object sender, EventArgs e)
+        {
+            if(testDataList.Count==0)
+            {
+                MessageBox.Show("暂无数据，请先执行批量测试");
+                return;
+            }
+            try
+            {
+                string fileName = DateTime.Now.ToString("HH-mm-ss") + "_测试报告.csv";
+                string fullPath = Path.Combine(GlobalLogPath, fileName);
+                using (StreamWriter sw = new StreamWriter(fullPath,false,Encoding.UTF8))
+                {
+                    //写入CSV表头（逗号分隔）
+                    sw.WriteLine("模块编号,电压（V）,温度（℃）,测试结果");
+                    //循环写入每条测试数据
+                    foreach(string[] dataRow in testDataList )
+                    {
+                        sw.WriteLine($"{dataRow[0]},{dataRow[1]},{dataRow[2]},{dataRow[3]}");
+                    }
+                }
+                MessageBox.Show($"CSV报告导出成功！可使用Excel打开\n路径：{fullPath}", "导出完成");
+                AddLog($"已导出CSV测试报告：{fileName}");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"导出失败{ex.Message}", "错误");
+            }
+        }
+    }
+}
